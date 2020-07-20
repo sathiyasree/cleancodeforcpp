25e5c1fddd43101be6fd9268a459326922a2cc92
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-07-20 15:43:01 +0530

    Updated unused variables

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,30 +1,26 @@
 
 bool checkBpm(float bpm)
 {
-  bool retVal = true;
    if(bpm < 70 || bpm > 150) {
-    retVal = false;
+    return false;
   }
-  return retVal;
+  return true;
 }
 bool checkSp02(float spo2)
 {
-  bool retVal = true;
   if(spo2 < 80) {
-    retVal = false;
+    return false;
   }
-  return retVal;
+  return true;
 }
 bool checkRespRate(float respRate)
 {
-  bool retVal = true;
   if(respRate < 30 || respRate > 60) {
-    retVal = false;
+   return false;
   }
-  return retVal;
+  return true;
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  bool retVal = true;
-  retVal = checkBpm(bpm)&& checkSp02(spo2) && checkRespRate(respRate);
+  bool retVal = checkBpm(bpm)&& checkSp02(spo2) && checkRespRate(respRate);
   return retVal;
 }
