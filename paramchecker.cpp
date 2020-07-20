97194261c466fec4eba1d80c041d19108d27d486
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-07-20 15:35:19 +0530

    Split the ifs to modular functions

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,30 @@
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  //int i;
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
+bool checkBpm(float bpm)
+{
+  bool retVal = true;
+   if(bpm < 70 || bpm > 150) {
+    retVal = false;
   }
-  return true;
+  return retVal;
+}
+bool checkSp02(float spo2)
+{
+  bool retVal = true;
+  if(spo2 < 80) {
+    retVal = false;
+  }
+  return retVal;
+}
+bool checkRespRate(float respRate)
+{
+  bool retVal = true;
+  if(respRate < 30 || respRate > 60) {
+    retVal = false;
+  }
+  return retVal;
+}
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  bool retVal = true;
+  retVal = checkBpm(bpm)&& checkSp02(spo2) && checkRespRate(respRate);
+  return retVal;
 }
