5a69b6477d208dc69767c2a26ae191ead89abc7b
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-07-22 15:00:14 +0530

    Updated test input type and test added

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,12 +1,19 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
  
-TEST(VitalsTest, BPM) { 
-    ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
+TEST(VitalsTest, BPM) {
+ std::vector<VitalCheckDetails> testInput = {{C_BPM ,100},{C_SPO2,100},{C_RESPRATE,50}};
+    ASSERT_EQ(true, vitalsAreOk(testInput));
 }
  
 TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
+ std::vector<VitalCheckDetails> testInput = {{C_BPM ,100},{C_SPO2,40},{C_RESPRATE,50}};
+    ASSERT_EQ(false, vitalsAreOk(testInput));
+}
+
+TEST(VitalsTest, SPO2_RESPRATE) {
+ std::vector<VitalCheckDetails> testInput = {{C_BPM ,100},{C_SPO2,40},{C_RESPRATE,61}};
+    ASSERT_EQ(false, vitalsAreOk(testInput));
 }
  
 int main(int argc, char **argv) {
