48c0c29a1baabb039f1ad11819879b4ccb634d7a
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-07-22 14:26:12 +0530

    updated file for open-close principle

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,25 @@
+#pragma once
+#include <vector>
+// Update list of possible parameters
+typedef enum VitalCheckParamType
+{
+	C_BPM = 0,
+	C_SPO2,
+	C_RESPRATE,
+};
 
-bool vitalsAreOk(float bpm, float spo2, float respRate);
+// to store the min and max of each parameter
+struct VitalBoundaryValues
+{
+	float min;
+	float max;
+};
+
+// type of input and value to be checked
+struct VitalCheckDetails
+{
+	VitalCheckParamType paramType;
+	float value;
+};
+
+bool vitalsAreOk(std::vector<VitalCheckDetails> vitalDetails);
