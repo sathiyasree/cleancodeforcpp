461025fe5aac5edb1d144318d3181f87e51e7c90
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-07-23 15:09:38 +0530

    Added result which says if higher, lower or normal

--- paramchecker.h
+++ paramchecker.h
@@ -1,11 +1,12 @@
 #pragma once
-#include <vector>
+#include <map>
 // Update list of possible parameters
+
 typedef enum VitalCheckParamType
 {
 	C_BPM = 0,
 	C_SPO2,
-	C_RESPRATE,
+	C_RESPRATE
 };
 
 // to store the min and max of each parameter
@@ -15,11 +16,35 @@ struct VitalBoundaryValues
 	float max;
 };
 
+// Create a lookup table with paramtype as key
+// Add boundary values for all possible parameters
+std::map<VitalCheckParamType, VitalBoundaryValues> vitalCheckBoundaryValues =
+{
+	{ C_BPM,{ 70,150 } },
+	{ C_SPO2,{ 80,100 } },
+	{ C_RESPRATE,{ 30,60 } }
+};
+
 // type of input and value to be checked
 struct VitalCheckDetails
 {
 	VitalCheckParamType paramType;
 	float value;
+};
+
+// vital check results
+typedef enum VitalCheckResultType
+{
+	C_NORMAL = 0,
+	C_LESSER,
+	C_HIGHER
+};
+
+struct VitalCheckResult
+{
+	VitalCheckParamType paramType;
+	VitalCheckResultType result;
+	VitalCheckResult(VitalCheckParamType paramType, VitalCheckResultType result) :paramType(paramType), result(result) {}
 };
 
 bool vitalsAreOk(std::vector<VitalCheckDetails> vitalDetails);
