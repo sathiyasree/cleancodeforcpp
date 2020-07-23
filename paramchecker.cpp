fece916867ca666564f51411d4ceb73b131963c4
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-07-23 15:12:17 +0530

    added parameter to say if vital normal

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,36 +1,36 @@
-
-#include <map>
 #include <algorithm>
 #include "paramchecker.h"
 
-// Create a lookup table with paramtype as key
-// Add boundary values for all possible parameters
-std::map<VitalCheckParamType, VitalBoundaryValues> vitalCheckBoundaryValues=
-{
-	{C_BPM, { 70,150 }},
-	{C_SPO2,{ 80,100 } },
-	{C_RESPRATE,{ 30,60 } }
-};
 
-bool checkmin(float rcvValue, float min)
+VitalCheckResultType checkmin(float rcvValue, float min)
 {
 	// -0.1 for float comparison
-	return (rcvValue < (min - 0.1));
+	return (rcvValue < (min - 0.1) ? C_LESSER : C_NORMAL);
 }
 
-bool checkmax(float rcvValue, float max)
+VitalCheckResultType checkmax(float rcvValue, float max)
 {
 	// +0.1 for float comparison
-	return (rcvValue > (max + 0.1));
+	return (rcvValue >(max + 0.1) ? C_HIGHER : C_NORMAL);
 }
 
-// Receive list of parameter and value to be checked
+VitalCheckResult checkvitalParamRange(VitalCheckDetails vitaldetail)
+{
+	VitalCheckResultType checkresult;
+	checkresult = checkmin(vitaldetail.value, vitalCheckBoundaryValues[vitaldetail.paramType].min);
+	if (checkresult != C_LESSER)
+	{
+		checkresult = checkmax(vitaldetail.value, vitalCheckBoundaryValues[vitaldetail.paramType].max);
+	}
+	return (VitalCheckResult(vitaldetail.paramType, checkresult));
+}
+
 bool vitalsAreOk(std::vector<VitalCheckDetails> vitalDetails)
 {
-	std::vector<bool> result;
+	std::vector<VitalCheckResult> result;
 	for (auto vital : vitalDetails)
-	{
-		result.emplace_back(checkmin(vital.value, vitalCheckBoundaryValues[vital.paramType].min) || checkmax(vital.value, vitalCheckBoundaryValues[vital.paramType].max));
+	{		
+		result.emplace_back(checkvitalParamRange(vital));
 	}
-    return std::none_of(result.begin(), result.end(), [](bool result) {return result != false;});
+    return std::none_of(result.begin(), result.end(), [](VitalCheckResult result) {return result.result != false;});
 }
